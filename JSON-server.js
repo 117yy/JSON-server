31f6eb2bd4ff79e19bb92df936b6cb0fa5db350b
117yy <yui593ktoy@gmail.com>
2021-02-06 12:32:06 +0900

    add files

---
+++ JSON-server.js
@@ -0,0 +1,22 @@
+const   http = require('http');
+const   url = require('url');
+if (process.argv.length == 3)
+{
+    port = parseInt(process.argv[2]);
+    try {
+var server = http.createServer((req, res) => {
+const URL = url.parse(req.url);
+user = url.parse(req.url, true).query.user;
+var json;
+json =
+'path : ' + URL.pathname + '\n' +
+'user : ' + user + '\n';
+res.end(json);
+});
+server.listen(port).on('error', (err) => {
+console.log(err.message);
+});
+    } catch(err) {
+console.log(err.message);
+    };
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # JSON-server
 http server template that respond JSON to a GET request
+# Usage
+1. node JSON-server.js (port)
+- ex. node JSON-server.js 8080&
+- Invalid number of command line arguments is not allowed.
+
+2. curl 'localhost:8080/path?user=test-user'
+- ex. curl 'localhost:8080/(pathname)?user=(username)'
+
+# Clean
+1. ps
+2. kill (PID of JSON-server.js)
